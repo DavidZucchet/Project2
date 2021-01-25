35a1281e9c3c14828493d29c1523af7791d0369a
David Zucchet Cardenas <davidzucchet@gmail.com>
2021-01-24 19:27:34 -0500

    adding files for project 2

---
+++ README.md
@@ -0,0 +1,45 @@
+# Disaster Response Pipeline Project
+
+# Table of Contents
+1. Installation
+2. Project Motivation
+3. File Descriptions
+4. Results
+5. Licensing, Authors, and Acknowledgements
+
+# Installation
+There should be no necessary libraries to run the code here beyond the Anaconda distribution of Python. The code should run with no issues using Python versions 3.*.
+
+# Project Motivation
+For this project, I was interestested in using the data provides by Figure Eight to build a model for an API that classifies disastes messages and can send them to an appropriate disaster relief agency.
+
+# File Descriptions
+There is 3 folders containing the data, models and the app. 
+The Data folder contains 2 CSV files with the raw data. The python file "process_data.py" has the code for extract, transform, clean and load the data.
+The models folder contains a python file "train_classifier.py". When executing this code it splits the data into a training set and a test set. Then, it creates a machine learning pipeline that uses NLTK, as well as scikit-learn's Pipeline and RandomSearchCV (RandomizedSearchCV is used instead of GridSearchCV because sacrifice in precision and accuracy is not huge in relation to processing time and finding best parameters) to output a final model that uses the message column to predict classifications for 36 categories (multi-output classification). Finally, it exports the model to a pickle file.
+The app folder contains a python file "run.py" that use the database and the model, and generate a web app using flask that displays data visualization and classify messages. It also has a folder with 2 html files for generating the web app. 
+Comments were used to assist in walking through the thought process for individual steps.
+
+# Results
+
+### Instructions:
+1. Run the following commands in the project's root directory to set up your database and model.
+
+    - To run ETL pipeline that cleans data and stores in database
+        `python data/process_data.py data/disaster_messages.csv data/disaster_categories.csv data/DisasterResponse.db`
+    - To run ML pipeline that trains classifier and saves
+        `python models/train_classifier.py data/DisasterResponse.db models/classifier.pkl`
+
+2. Run the following command in the app's directory to run your web app.
+    `python run.py`
+
+3. Go to http://0.0.0.0:3001/
+
+The results of the model can be found at the web app.
+
+# Licensing, Authors, Acknowledgements
+Must give credit to Figure Eight for the data and Udacity for the learning path in the DataScientist Course and some of the code that I reused. Otherwise, feel free to use the code here as you would like!
+
+
+
+
---
+++ models/train_classifier.py
@@ -0,0 +1,96 @@
+import sys
+# import libraries
+from sqlalchemy import create_engine
+import pandas as pd
+import numpy as np
+import re
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from sklearn.metrics import confusion_matrix
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.metrics import classification_report
+from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
+import nltk
+nltk.download(['punkt', 'wordnet'])
+import pickle
+
+def load_data(database_filepath):
+    # load data from database
+    engine = create_engine('sqlite:///'+database_filepath)
+    df = pd.read_sql_table("Messages", engine)
+    X = df.message.values
+    y = df.iloc[:, 4:].values
+    return X, y, df.columns
+
+def tokenize(text):
+    #tokenization function to process your text data
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens
+
+
+def build_model():
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultiOutputClassifier(RandomForestClassifier()))
+    ])
+    parameters = {
+        'vect__max_df': (0.5, 0.75, 1.0),
+        'tfidf__use_idf': (True, False),
+        'clf__estimator__min_samples_split': [2, 3,4]
+    }
+    # RandomizedSearchCV is used instead of GridSearchCV because sacrifice in precision and accuracy is not huge in relation to processing time and finding best parameters
+    cv = RandomizedSearchCV(pipeline, param_distributions=parameters,n_jobs= -1)
+    
+    return cv
+def evaluate_model(model, X_test, Y_test, category_names):
+    
+    y_pred = model.predict(X_test)
+    for i in range(y_pred.shape[1]):
+        print(category_names[i])
+        print(classification_report(Y_test[:,i],y_pred[:,i]))
+
+def save_model(model, model_filepath):
+    pickle.dump(model.best_estimator_, open(model_filepath, 'wb'))
+
+def main():
+    if len(sys.argv) == 3:
+        database_filepath, model_filepath = sys.argv[1:]
+        print('Loading data...\n    DATABASE: {}'.format(database_filepath))
+        X, Y, category_names = load_data(database_filepath)
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+        
+        print('Building model...')
+        model = build_model()
+        
+        print('Training model...')
+        model.fit(X_train, Y_train)
+        
+        print('Evaluating model...')
+        evaluate_model(model, X_test, Y_test, category_names)
+
+        print('Saving model...\n    MODEL: {}'.format(model_filepath))
+        save_model(model, model_filepath)
+
+        print('Trained model saved!')
+
+    else:
+        print('Please provide the filepath of the disaster messages database '\
+              'as the first argument and the filepath of the pickle file to '\
+              'save the model to as the second argument. \n\nExample: python '\
+              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
+
+
+if __name__ == '__main__':
+    main()
